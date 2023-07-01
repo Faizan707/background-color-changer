481cc71da6e116a4192f29cec4eab8bcfb2ea831
Faizan707 <faizanbutt707@gmail.com>
2023-07-01 06:47:00 +0500

    hoho

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("btn");
+let body = document.querySelector("body");
+
+btn.addEventListener('click', () => {
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  var alpha = Math.random().toFixed(2); // Random alpha value between 0 and 1, rounded to 2 decimal places
+
+  // Construct the RGBA color string
+  var rgbaColor = "rgba(" + red + ", " + green + ", " + blue + ", " + alpha + ")";
+
+  // Apply the generated color to the background color of the body
+  body.style.backgroundColor = rgbaColor;
+  btn.style.backgroundColor=rgbaColor
+});
