6bb76ccbab6b7069d5822020eec4d7f1b6da6912
Faizan707 <faizanbutt707@gmail.com>
2023-07-02 08:11:34 +0500

    hoho

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ btn.addEventListener('click', () => {
   var red = Math.floor(Math.random() * 256);
   var green = Math.floor(Math.random() * 256);
   var blue = Math.floor(Math.random() * 256);
-  var alpha = Math.random().toFixed(2); // Random alpha value between 0 and 1, rounded to 2 decimal places
+  var alpha = Math.random().toFixed(1); // Random alpha value between 0 and 1, rounded to 2 decimal places
 
   // Construct the RGBA color string
   var rgbaColor = "rgba(" + red + ", " + green + ", " + blue + ", " + alpha + ")";
